abb1c9f2edc187c394278f18027b5b5b9e98d79a
Anjanah Meenakshi <126347598+anjanahkarthic@users.noreply.github.com>
2025-04-01 19:05:24 +0530

    Update and rename rabbitmq.sh to rabbitmq.yaml

--- rabbitmq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p /usr/local/src/rabbitmq
-
-cd /usr/local/src/rabbitmq && wget https://raw.githubusercontent.com/anjanahkarthic/rabbitmq/refs/heads/main/installation.yaml
-cd /usr/local/src/rabbitmq && wget https://raw.githubusercontent.com/anjanahkarthic/rabbitmq/refs/heads/main/plugin.yaml
-cd /usr/local/src/rabbitmq && wget https://raw.githubusercontent.com/anjanahkarthic/rabbitmq/refs/heads/main/user.yaml
